4f02f557cfef3c03d7ff4f7bd1e7ef70aa01581f
Lucas <lucas.caetano.reis@hotmail.com>
2026-04-26 23:24:09 -0300

    Métodos para promises

---
+++ aula12/index.js
@@ -0,0 +1,36 @@
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, tempo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof msg !== "string") {
+        reject("CAI NO ERRO");
+        return;
+      }
+
+      resolve(msg.toUpperCase() + " - Passei na promise");
+      return;
+    }, tempo);
+  });
+}
+
+// Promise.all Promise.race Promise.resolve Promise.reject
+function baixaPagina() {
+  const emCache = true;
+
+  if (emCache) {
+    return Promise.resolve("Página em chace.");
+  } else {
+    return esperaAi("Baixei a página", 3000);
+  }
+}
+
+baixaPagina()
+  .then((dadosPagina) => {
+    console.log(dadosPagina);
+  })
+  .catch((e) => console.log("ERRO", e));
